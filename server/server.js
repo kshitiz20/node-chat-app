efd9abb6f3999ec7e80d0385c1b0a1ee6e60bb7f
kshitizsharma62@gmail.com <kshitizsharma62@gmail.com>
2019-01-16 23:56:33 +0530

    setting up a socket connection between client and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const path= require('path');
+var http= require('http');
+var socketIO= require('socket.io');
 
 var app= express();
 
@@ -9,10 +11,19 @@ var publicPath= path.join(__dirname,'../public');
 
 app.use(express.static(publicPath));
 
-// app.get('/',(req, res)=>{
-//     res.send()
-// })
+var server=  http.createServer(app);
+var io= socketIO(server);
 
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+    console.log('Tera baap aaya hai');
+
+    socket.on('disconnect',()=>{
+        console.log('Tera baap wps gaya');
+    })
+})
+
+
+server.listen(port,()=>{
     console.log('Bhai tera full set hai at port 3000');
 })
