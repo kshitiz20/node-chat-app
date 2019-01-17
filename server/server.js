3127427776cda0f573d9c22ec811a543a7c5246a
kshitizsharma62@gmail.com <kshitizsharma62@gmail.com>
2019-01-17 23:51:17 +0530

    Adding greetong message to new Users

--- server/server.js
+++ server/server.js
@@ -22,7 +22,24 @@ io.on('connection',(socket)=>{
         console.log('Tera baap wps gaya');
     })
 
-  
+
+    //soket events are entented for a single user
+    //io events are for all the connected users
+    //socket.broadcast.emit events are for all the connected users except the one who initiated the event
+
+
+    //This message will be shown tp the user who joined
+    socket.emit("newMessage", {
+        from:"Admin",
+        text:"Welcome to the chat App"
+    })
+
+
+    //This message will be shown to the all the other users when the new user joins
+    socket.broadcast.emit("newMessage",{
+        from:"Admin",
+        text:"New User joined"
+    })
     socket.on("createMessage", (message)=>{
         console.log("New Message", message);
         io.emit("newMessage", {
