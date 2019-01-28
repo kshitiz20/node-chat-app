890aab94dfe6c610cda5ded1f34051e5bfd4802b
kshitizsharma62@gmail.com <kshitizsharma62@gmail.com>
2019-01-29 00:36:56 +0530

    Sending Users to specific rooms

--- server/server.js
+++ server/server.js
@@ -50,16 +50,19 @@ io.on('connection',(socket)=>{
     })
 
     socket.on("createMessage", (message, callback)=>{
-        console.log("New Message", message);
+        var user= users.getUser(socket.id);
+        if(user&&isRealString(message.text))
         //emits to everyone
-        io.emit("newMessage",generateMessage(message.from, message.text));
+        io.to(user.room).emit("newMessage",generateMessage(user.name, message.text));
         callback();
     })
 
     socket.on("createLocationMessage", (coords)=>{
 
         console.log(coords);
-        io.emit("newLocationMessage", generateURLMessage('Admin',coords.latitude,coords.longitude));
+        var user= users.getUser(socket.id);
+        if(user)
+        io.to(user.room).emit("newLocationMessage", generateURLMessage(user.name,coords.latitude,coords.longitude));
     })
 
     socket.on('disconnect',()=>{
