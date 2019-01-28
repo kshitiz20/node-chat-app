197f76fd79d167f13c89c6023e5e609904c40e75
kshitizsharma62@gmail.com <kshitizsharma62@gmail.com>
2019-01-29 00:06:00 +0530

    Wiring up users list

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const path= require('path');
 var http= require('http');
 var socketIO= require('socket.io');
 var {generateMessage, generateURLMessage}= require('./utils/message.js');
+var {isRealString}= require('./utils/validate.js')
+var {Users}= require('./utils/user');
 
+var users= new Users();
 var app= express();
 
 var port= process.env.PORT||3000;
@@ -19,23 +22,32 @@ var io= socketIO(server);
 
 io.on('connection',(socket)=>{
     console.log('Tera baap aaya hai');
-
-    socket.on('disconnect',()=>{
-        console.log('Tera baap wps gaya');
-    })
-
-
     //soket events are entented for a single user
     //io events are for all the connected users
     //socket.broadcast.emit events are for all the connected users except the one who initiated the event
 
 
-    //This message will be shown tp the user who joined
-    socket.emit("newMessage",generateMessage('Admin', 'Welcome to the chat App'));
+   
+    socket.on('join', (params, callback)=>{
+        if(!isRealString(params.name)||!isRealString(params.room)){
+
+          return  callback('Name and Room to join are required');
+        }
+       socket.join(params.room);
+       users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 
 
-    //This message will be shown to the all the other users when the new user joins
-    socket.broadcast.emit("newMessage",generateMessage('Admin', 'New User Connected'))
+        //This message will be shown tp the user who joined
+        socket.emit("newMessage",generateMessage('Admin', 'Welcome to the chat App'));
+
+
+        //This message will be shown to the all the other users when the new user joins
+        socket.broadcast.to(params.room).emit("newMessage",generateMessage('Admin', `${params.name} Connected`))
+        callback();
+    })
 
     socket.on("createMessage", (message, callback)=>{
         console.log("New Message", message);
@@ -49,6 +61,16 @@ io.on('connection',(socket)=>{
         console.log(coords);
         io.emit("newLocationMessage", generateURLMessage('Admin',coords.latitude,coords.longitude));
     })
+
+    socket.on('disconnect',()=>{
+        var user= users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the room !`));
+        }
+        console.log('Tera baap wps gaya');
+    })
+
 })
 
 
--- server/utils/user.js
+++ server/utils/user.js
@@ -4,48 +4,42 @@ class Users {
     constructor(){
         this.users=[];
     }
-    addUser(id, name, room){
-        var user= {id, name, room};
+    addUser (id, name, room) {
+        var user = {id, name, room};
         this.users.push(user);
         return user;
-    }
-
-    removeUser(id){
-         this.users= _.remove(this.users, (user)=>{
-            return user.id===id;
-        })
-        return this.users;
-
-    }
-    getUser(id){
-        var user= _.find(this.users, (user)=>{
-            return user.id===id;
-        })
-        return user
-    }
-    getUserList(room){
-        var usersInRoom= _.filter(this.users, function(user){
-            return user.room===room;
-        })
-
-        var namesArray= _.map(usersInRoom, (user)=>{
-            return user.name
-        })
+      }
+      removeUser (id) {
+        var user = this.getUser(id);
+    
+        if (user) {
+          this.users = this.users.filter((user) => user.id !== id);
+        }
+    
+        return user;
+      }
+      getUser (id) {
+        return this.users.filter((user) => user.id === id)[0]
+      }
+      getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+    
         return namesArray;
-    }
+      }
 
 }
 
-var UserInstance= new Users();
-UserInstance.addUser('123','Kshitiz', 'Dudes');
-UserInstance.addUser('456','Aman', 'Dudes');
-UserInstance.addUser('789','Amit', 'Dudes');
-UserInstance.addUser('1010','Ajay', 'Dudes');
+// var UserInstance= new Users();
+// UserInstance.addUser('123','Kshitiz', 'Dudes');
+// UserInstance.addUser('456','Aman', 'Dudes');
+// UserInstance.addUser('789','Amit', 'Dudes');
+// UserInstance.addUser('1010','Ajay', 'Dudes');
 
-UserInstance.addUser('abc','Vanshika', 'Cool');
-UserInstance.addUser('def','Vaibhavi', 'Cool');
-UserInstance.addUser('fgh','Anita', 'Cool');
-UserInstance.addUser('jkl','Akshaya', 'Cool');
+// UserInstance.addUser('abc','Vanshika', 'Cool');
+// UserInstance.addUser('def','Vaibhavi', 'Cool');
+// UserInstance.addUser('fgh','Anita', 'Cool');
+// UserInstance.addUser('jkl','Akshaya', 'Cool');
 
 // console.log(UserInstance.users);
 // console.log(UserInstance.addUser('555','John', 'Dusky'));
@@ -53,6 +47,8 @@ UserInstance.addUser('jkl','Akshaya', 'Cool');
 // console.log(UserInstance.users);
 // console.log(UserInstance.removeUse('456'));
 // console.log(UserInstance.users);
-console.log(UserInstance.getUser('123'));
-console.log(UserInstance.getUserList('Cool'));
+// console.log(UserInstance.getUser('123'));
+// console.log(UserInstance.getUserList('Cool'));
+// console.log(UserInstance.removeUser('123'));
+// console.log(UserInstance.getUserList('Dudes'))
 module.exports={Users};
